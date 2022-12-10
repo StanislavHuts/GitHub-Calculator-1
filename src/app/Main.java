3f4ddf06e1fe02be35c804b2ca0bfcc6b7a3116f
StanislavHuts <stashuts@gmail.com>
2022-12-10 13:39:04 +0200

    Init out Main

--- src/app/Main.java
+++ src/app/Main.java
@@ -1,12 +1,11 @@
 package app;
 
 public class Main {
-
+    static int a = 8;
+    static int b = 2;
     public static void main(String[] args) {
         System.out.println("My app for Users!");
         System.out.println("Version 1.0");
-        int a = 8;
-        int b = 2;
         int c = add(a, b);
         System.out.println(c);
         int d = multiply(a, b);
@@ -14,7 +13,7 @@ public class Main {
         int e = division(a, b);
         System.out.println(e);
         int f = subtract(a, b);
-        System.out.println(d);
+        System.out.println(f);
     }
 
     private static int add(int a, int b) {
